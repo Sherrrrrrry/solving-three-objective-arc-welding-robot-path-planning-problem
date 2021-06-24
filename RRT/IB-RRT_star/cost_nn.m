c299b6aebc8dc1871a2061f5972a30bf54393585
Sherrrrrrry <45441877+Sherrrrrrry@users.noreply.github.com>
2021-06-24 16:51:38 +0800

    Delete RRT/IB-RRT_star directory

--- RRT/IB-RRT_star/cost_nn.m
+++
@@ -1,8 +0,0 @@
-%calculate the cost from a node to a node
-function [cost] = cost_nn(from_node,to_node,dim)
-
-diff = from_node(:,1:dim) - to_node(:,1:dim);
-eucl_dist = norm(diff);
-cost = from_node(:,dim+2) + eucl_dist;
-
-end
--- RRT/IB-RRT_star/line_cost.m
+++
@@ -1,4 +0,0 @@
-function [cost] = line_cost(from_node,to_point,dim)
-diff = from_node(:,1:dim) - to_point;
-cost = norm(diff);
-end
